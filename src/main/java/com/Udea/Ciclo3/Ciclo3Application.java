26d44f35c7a19abd871395c60782df573f6dabeb
Brandon Ospino <ospinotech@gmail.com>
2022-09-05 18:20:30 -0500

    Creación de rest controller. Primera prueba para mandar mensaje a la Web.

--- src/main/java/com/Udea/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/Udea/Ciclo3/Ciclo3Application.java
@@ -2,9 +2,19 @@ package com.Udea.Ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
+@RestController
 public class Ciclo3Application {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Primera prueba";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
